fb8a69cf76f80aa1dc7166466e3991e05d9e53a2
Theo Larrieu <theo@jlab.org>
2018-04-23 15:41:27 -0400

    publish views too

--- src/TaxonomyServiceProvider.php
+++ src/TaxonomyServiceProvider.php
@@ -15,9 +15,12 @@ class TaxonomyServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
-        //$this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/../resources/views','taxonomy');
 
+        /*
+         * Routes
+         */
+        $this->loadRoutesFrom(__DIR__ . '/../routes/api.php');
         // Must include the bindings middleware in order to get 
         // route model binding in a package like this.
         Route::group([
@@ -28,12 +31,22 @@ class TaxonomyServiceProvider extends ServiceProvider
             $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
         });
 
-        $this->loadViewsFrom(__DIR__ . '/../resources/views','taxonomy');
 
+        /*
+         * Items we publish so that the user can override in his/her own application
+         */
+        $this->publishes([
+            __DIR__ . '/../resources/views' => resource_path('views/vendor/jlab/taxonomy'),
+        ]);
         $this->publishes([
             __DIR__.'/../resources/assets/js/' => public_path('vendor/jlab/taxonomy/js/'),
         ], 'public');
-        
+
+
+        /*
+         * Publish our factory extensions for the convenience of a customer who wants to test
+         * models with taxonomy dependencies.
+         */
         $this->app->make('Illuminate\Database\Eloquent\Factory')->load(__DIR__ . '/../database/factories');
     }
 
