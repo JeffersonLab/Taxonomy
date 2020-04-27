20e3c1db6b0556c2191f097e1b4e97a10f716ae4
theo <theo@jlab.org>
2020-04-27 12:45:12 -0400

    namespace the VocabularyController resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,13 @@
 <?php
 
-Route::resource('taxonomy/vocabularies','Jlab\Taxonomy\Http\Controllers\VocabularyController');
+Route::resource('taxonomy/vocabularies','Jlab\Taxonomy\Http\Controllers\VocabularyController')
+    ->names([
+        'index' => 'jlab.taxonomy.vocabulary.index',
+        'store' => 'jlab.taxonomy.vocabulary.store',
+        'create' => 'jlab.taxonomy.vocabulary.create',
+        'show' => 'jlab.taxonomy.vocabulary.show',
+        'destroy' => 'jlab.taxonomy.vocabulary.destroy',
+        'update' => 'jlab.taxonomy.vocabulary.update',
+        'edit' => 'jlab.taxonomy.vocabulary.edit',
+
+    ]);;
