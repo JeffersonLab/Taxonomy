cddfc03d04aed204c995f427129499e8f6da696a
theo <theo@jlab.org>
2020-05-05 13:13:53 -0400

    Remove column_class which really didn't belong.

--- database/migrations/2018_04_12_090732_create_vocabularies_table.php
+++ database/migrations/2018_04_12_090732_create_vocabularies_table.php
@@ -18,11 +18,6 @@ class CreateVocabulariesTable extends Migration
             $table->string('name',80);
             $table->string('description',1000)->nullable();
 
-            // We will allow the vocabulary owner to tweak the number
-            // of columns used when the vocabulary is presented in a
-            // form as checkboxes.
-            $table->string('column_class')->nullable();
-
             $table->unique('name');
 
         });
